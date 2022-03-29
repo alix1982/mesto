b62ddd07dfcb9c8176f338b93d60f8fdba093552
Alix <alix1982@yandex.ru>
2022-03-29 21:25:02 +0300

    Проектная работа 3.5c.3

--- scripts/script.js
+++ scripts/script.js
@@ -27,8 +27,7 @@ const initialCards = [
 ]; 
 
 let element = document.querySelector('.element');
-let card = document.querySelector('#foto').content;
-let buttonElementDel = document.querySelectorAll('.element__del');
+const card = document.querySelector('#foto').content;
 
 for (let i = 0; i<initialCards.length; i++) {
   let cardOnline = card.querySelector('.element__list').cloneNode(true);
@@ -41,71 +40,76 @@ for (let i = 0; i<initialCards.length; i++) {
 };
 //
 
-//  попапы Info и Add
-let popupWin = document.querySelector('.popup');
-let close = document.querySelector('.popup__close');
-let form = document.querySelector('.form');
-let heading = document.querySelector('.popup__heading');
-let input = document.querySelectorAll('.form__text');
-let save = document.querySelector('.form__save');
+//  попап Info
+let popupWinInfo = document.querySelector('.popupInfo');
+let inputInfo = document.querySelectorAll('.formInfo__text');
 let name = document.querySelector('.profile__name');
 let work = document.querySelector('.profile__work');
-let buttonProfileInfo = document.querySelector('.profile__info-button');
-let buttonProfileAdd = document.querySelector('.profile__add-button');
+const closeInfo = document.querySelector('.popupInfo__close');
+const formInfo = document.querySelector('.formInfo');
+const buttonProfileInfo = document.querySelector('.profile__info-button');
 
-function closePopup () {
-  popupWin.classList.remove('popup_opened');
-  popupWin.style = 'transition: visibility 1s, opacity 1s linear;'
-}
-
-function openPopup () {
-  popupWin.classList.add('popup_opened');
-  popupWin.style = 'transition: visibility 0s, opacity 1s linear;'
-}
-
+function closePopupInfo () {
+  popupWinInfo.classList.remove('popupInfo_opened');
+  popupWinInfo.style = 'transition: visibility 1s, opacity 1s linear;'
+};
+function openPopupInfo () {
+  popupWinInfo.classList.add('popupInfo_opened');
+  popupWinInfo.style = 'transition: visibility 0s, opacity 1s linear;'
+};
 function formOpenInfo () {
-  heading.textContent = 'Редактировать профиль';
-  input[0].value = (name.textContent);
-  input[1].value = (work.textContent);
-  input[0].placeholder = 'Жак-Ив Кусто';
-  input[1].placeholder = 'Иссдедователь океана';
-  save.textContent = 'Сохранить';
-  openPopup();
-}
-
-function formOpenAdd () {
-  heading.textContent = 'Новое место';
-  input[0].value = '';
-  input[1].value = '';  
-  input[0].placeholder = 'Название';
-  input[1].placeholder = 'Ссылка на картинку'; 
-  save.textContent = 'Создать';
-  openPopup();
+  inputInfo[0].value = (name.textContent);
+  inputInfo[1].value = (work.textContent);
+  openPopupInfo();
+};
+function formSubmitInfo (evt) {
+  evt.preventDefault();
+  name.textContent = inputInfo[0].value;
+  work.textContent = inputInfo[1].value;
+  closePopupInfo();
 };
 
-function formSubmit (evt) {
+buttonProfileInfo.addEventListener('click', formOpenInfo);
+closeInfo.addEventListener('click', closePopupInfo);
+formInfo.addEventListener('submit', formSubmitInfo);
+//
+
+
+//  попап Add
+let popupWinAdd = document.querySelector('.popupAdd');
+let inputAdd = document.querySelectorAll('.formAdd__text');
+const buttonProfileAdd = document.querySelector('.profile__add-button');
+const closeAdd = document.querySelector('.popupAdd__close');
+const formAdd = document.querySelector('.formAdd');
+
+function closePopupAdd () {
+  popupWinAdd.classList.remove('popupAdd_opened');
+  popupWinAdd.style = 'transition: visibility 1s, opacity 1s linear;'
+}
+function openPopupAdd () {
+  popupWinAdd.classList.add('popupAdd_opened');
+  popupWinAdd.style = 'transition: visibility 0s, opacity 1s linear;'
+}
+function formOpenAdd () {
+  inputAdd[0].value = '';
+  inputAdd[1].value = '';
+  openPopupAdd();
+};
+function formSubmitAdd (evt) {
   evt.preventDefault();
-  if (heading.textContent === 'Новое место') {
-    let cardOnline = card.querySelector('.element__list').cloneNode(true);
-    cardOnline.querySelector('.element__img').src = input[1].value;
-    cardOnline.querySelector('.element__text').textContent = input[0].value;
-    like (cardOnline);
-    del (cardOnline);
-    openImg (cardOnline);
-    element.prepend(cardOnline);
-    closePopup();
-  }
-  else {
-    name.textContent = input[0].value;
-    work.textContent = input[1].value;
-    closePopup();
-  }
+  let cardOnline = card.querySelector('.element__list').cloneNode(true);
+  cardOnline.querySelector('.element__img').src = inputAdd[1].value;
+  cardOnline.querySelector('.element__text').textContent = inputAdd[0].value;
+  like (cardOnline);
+  del (cardOnline);
+  openImg (cardOnline);
+  element.prepend(cardOnline);
+  closePopupAdd();
 }
 
-buttonProfileInfo.addEventListener('click', formOpenInfo);
 buttonProfileAdd.addEventListener('click', formOpenAdd);
-close.addEventListener('click', closePopup);
-form.addEventListener('submit', formSubmit);
+closeAdd.addEventListener('click', closePopupAdd);
+formAdd.addEventListener('submit', formSubmitAdd);
 //
 
 
@@ -136,10 +140,10 @@ function openImg (evt) {
     popupWinImg.classList.add('popupImg_opened');
   });
 }
-
 function closePopupImg () {
   popupWinImg.classList.remove('popupImg_opened');
   popupWinImg.style = 'transition: visibility 1s, opacity 1s linear;'
 }
+
 closeImg.addEventListener('click', closePopupImg);
 //
